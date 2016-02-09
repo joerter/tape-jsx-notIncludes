94fcea3950dbabf0dd890444cbb9dd2f610a0440
John Oerter <joerter@gmail.com>
2016-02-09 10:13:03 -0600

    Add collapse white space for better comparison

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import reactElementToString from 'react-element-to-jsx-string';
+import collapse from 'collapse-white-space';
 
 export default function jsxNotIncludes (actual, expected, message) {
-  this.false(reactElementToString(expected).includes(reactElementToString(actual)), message);
+  this.false(collapse(reactElementToString(expected)).includes(collapse(reactElementToString(actual))), message);
 };
